74e843601623bee3ffa9c71d35b9395276ad83c3
James Cagalawan <james.cagalawan@gmail.com>
2024-09-28 17:39:42 -0400

    Remove zsh and set up fzf with ripgrep

--- default.nix
+++ default.nix
@@ -2,17 +2,22 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.zsh
-    pkgs.oh-my-zsh
     pkgs.ripgrep
     pkgs.fzf
   ];
 
   shellHook = ''
-    export SHELL=$(which zsh)
-    if [ -z "$ZSH" ]; then
-      echo "Setting up oh-my-zsh"
-      sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    fi
+  # Set up fzf key bindings and completion
+  source ${pkgs.fzf}/share/fzf/key-bindings.bash
+  source ${pkgs.fzf}/share/fzf/completion.bash
+
+  # Configure fzf for history search
+  export FZF_CTRL_R_OPTS="--sort --exact"
+
+  # Optional: Customize fzf command to use ripgrep
+  export FZF_DEFAULT_COMMAND='rg --files --hidden'
+
+  # Optional: Set a custom prompt to indicate you're in a Nix shell
+  export PS1="\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] "
   '';
 }
