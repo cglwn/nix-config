27744bcaa2995c602421bf50969bc916ef7ba936
James Cagalawan <james.cagalawan@gmail.com>
2024-09-28 18:57:34 -0400

    Enable mouse for Emacs in terminal

--- default.nix
+++ default.nix
@@ -1,9 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  myEmacsConfig = pkgs.writeText "my-emacs-config.el" ''
+    (custom-set-variables
+     '(xterm-mouse-mode t))
+  '';
+in
 pkgs.mkShell {
   buildInputs = [
     pkgs.ripgrep
     pkgs.fzf
+    pkgs.emacs
+    pkgs.git
   ];
 
   shellHook = ''
@@ -16,6 +24,10 @@ pkgs.mkShell {
 
   # Optional: Customize fzf command to use ripgrep
   export FZF_DEFAULT_COMMAND='rg --files --hidden'
+
+  # Set up Emacs with custom configuration
+  export EMACSLOADPATH="${pkgs.emacs}/share/emacs/site-lisp:"
+  alias emacs='emacs -q --load ${myEmacsConfig}'
 
   # Optional: Set a custom prompt to indicate you're in a Nix shell
   export PS1="\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] "
